7c75eef6c50476d4ac1a9043ee5ccb352ecff26e
Meryem Talay <meyremtlly@gmail.com>
2023-09-12 14:11:44 +0300

    metotlar

---
+++ metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace metotlar // Note: actual namespace depends on the project name.
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//erisim_belirteci geri_donuctipi metot_adi(parametrelistesi/arguman)
+			////komutlar;
+			// return;
+			//}
+			int a = 2;
+			int b = 3;
+			Console.WriteLine(a + b);
+
+			int sonuc = Topla(a, b);
+			Console.WriteLine(sonuc);
+			Methodlar ornek = new Methodlar();
+			ornek.EkranaYazdir(Convert.ToString(sonuc));
+			int sonuc2 = ornek.ArttirveTopla(ref a, ref b);
+			ornek.EkranaYazdir(Convert.ToString(sonuc2));
+			ornek.EkranaYazdir(Convert.ToString(a + b));
+		}
+		static int Topla(int deger1, int deger2)
+		{
+			return (deger1 + deger2);
+		}		
+	}
+	class Methodlar
+	{
+		public void EkranaYazdir(string veri)
+		{
+			Console.WriteLine(veri);
+		}
+		public int ArttirveTopla( ref int deger1, ref int deger2)
+		{
+			deger1 += 1;
+			deger2 += 1;
+			return (deger1 + deger2);
+		}
+	}
+}
