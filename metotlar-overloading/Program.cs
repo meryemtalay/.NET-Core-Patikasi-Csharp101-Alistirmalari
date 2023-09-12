743bc3f15f1735cf6dcb9702cb89132f86ca7fbc
Meryem Talay <meyremtlly@gmail.com>
2023-09-12 15:04:12 +0300

    metot overloading

---
+++ metotlar-overloading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace metotlar_overloading // Note: actual namespace depends on the project name.
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			//out parametreler
+			string sayi = "999";
+			bool sonuc = int.TryParse(sayi, out int outSayi); if (sonuc)
+			{
+				Console.WriteLine("Başarılı!"); Console.WriteLine(outSayi);
+			}
+			else
+			{
+			}
+			Console.WriteLine("Başarısız!");
+			Metotlar instance = new Metotlar();
+			instance.Topla(4, 5, out int toplamSonucu); Console.WriteLine(toplamSonucu);
+
+			//Metot Aşırı Yükleme - Overloading
+			int ifade = 999;
+			instance.EkranaYazdir(Convert.ToString(ifade));
+			instance.EkranaYazdir(ifade);
+
+			//Method İmzası
+			//methodAdı + Parametre Sayısı + Parametre Tipi
+		}
+	}
+
+	class Metotlar
+	{
+		public void Topla(int a, int b, out int toplam)
+		{
+			toplam = a + b;
+		}
+		public void EkranaYazdir(string veri)
+		{
+			Console.WriteLine(veri);
+		}
+		public void EkranaYazdir(int veri)
+		{
+			Console.WriteLine(veri);
+		}
+	}
+}
